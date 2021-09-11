2d1717badf24a5a3cc25eb99dc5b83feea05f3d8
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2021-09-11 16:44:06 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+import { getRefs } from "./js/getRefs";
 import css from "./css/styles.css";
 import menu from "./js/menu";
 import menuItems from "./template/menu.hbs";
+const refs = getRefs()
+
 const markeUp = menuItems(menu) 
 
-const menuRef = document.querySelector(".js-menu")
-console.log(menuRef)
+refs.menu.insertAdjacentHTML("beforeend", markeUp)
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-menuRef.insertAdjacentHTML("beforeend", markeUp)
+const userTheme = localStorage.getItem("theme")
+if (userTheme === 'dark-theme') {
+    refs.body.classList.add(Theme.DARK);
+    refs.input.checked = true
+} else {
+    refs.body.classList.add(Theme.LIGHT)
+}
+
+refs.input.addEventListener("change", () => {
+    refs.body.classList.toggle(Theme.DARK)
+    refs.body.classList.toggle(Theme.LIGHT)
+    changeTheme()
+
+})
+
+const changeTheme = () => {
+localStorage.setItem("theme", refs.input.checked ?Theme.DARK: Theme.LIGHT)
+}
